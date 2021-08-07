beb1f9f941e742abfe22a37a7768b57cfce9c71d
chrisd149 <christianmigueldiaz@gmail.com>
2021-08-07 11:12:26 -0400

    fixed transparent buttons

--- src/OpenTTLauncher.Designer.cs
+++ src/OpenTTLauncher.Designer.cs
@@ -55,10 +55,9 @@ namespace OpenTTLauncher
             this.linkLabel.BackColor = System.Drawing.Color.Transparent;
             this.linkLabel.Font = new System.Drawing.Font("Impress BT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.linkLabel.LinkColor = System.Drawing.Color.Navy;
-            this.linkLabel.Location = new System.Drawing.Point(314, 383);
-            this.linkLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.linkLabel.Location = new System.Drawing.Point(419, 471);
             this.linkLabel.Name = "linkLabel";
-            this.linkLabel.Size = new System.Drawing.Size(121, 20);
+            this.linkLabel.Size = new System.Drawing.Size(159, 25);
             this.linkLabel.TabIndex = 0;
             this.linkLabel.TabStop = true;
             this.linkLabel.Text = "Github repository";
@@ -71,43 +70,42 @@ namespace OpenTTLauncher
             this.loginButton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("loginButton.BackgroundImage")));
             this.loginButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.loginButton.ForeColor = System.Drawing.Color.Transparent;
-            this.loginButton.Location = new System.Drawing.Point(643, 338);
-            this.loginButton.Margin = new System.Windows.Forms.Padding(2);
+            this.loginButton.Location = new System.Drawing.Point(857, 416);
+            this.loginButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.loginButton.Name = "loginButton";
-            this.loginButton.Size = new System.Drawing.Size(90, 69);
+            this.loginButton.Size = new System.Drawing.Size(120, 85);
             this.loginButton.TabIndex = 2;
             this.loginButton.UseVisualStyleBackColor = false;
             this.loginButton.Click += new System.EventHandler(this.loginButton_Click);
             // 
             // usernameTextBox
             // 
+            this.usernameTextBox.Cursor = System.Windows.Forms.Cursors.Default;
             this.usernameTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.usernameTextBox.Location = new System.Drawing.Point(140, 137);
-            this.usernameTextBox.Margin = new System.Windows.Forms.Padding(2);
+            this.usernameTextBox.Location = new System.Drawing.Point(187, 169);
+            this.usernameTextBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.usernameTextBox.Name = "usernameTextBox";
-            this.usernameTextBox.Size = new System.Drawing.Size(459, 26);
+            this.usernameTextBox.Size = new System.Drawing.Size(611, 30);
             this.usernameTextBox.TabIndex = 4;
-            this.usernameTextBox.UseWaitCursor = true;
             this.usernameTextBox.TextChanged += new System.EventHandler(this.UsernameTextBox_TextChanged);
             // 
             // passwordTextBox
             // 
+            this.passwordTextBox.Cursor = System.Windows.Forms.Cursors.Default;
             this.passwordTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.passwordTextBox.Location = new System.Drawing.Point(140, 188);
-            this.passwordTextBox.Margin = new System.Windows.Forms.Padding(2);
+            this.passwordTextBox.Location = new System.Drawing.Point(187, 231);
+            this.passwordTextBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.passwordTextBox.Name = "passwordTextBox";
             this.passwordTextBox.PasswordChar = '*';
-            this.passwordTextBox.Size = new System.Drawing.Size(459, 26);
+            this.passwordTextBox.Size = new System.Drawing.Size(611, 30);
             this.passwordTextBox.TabIndex = 5;
-            this.passwordTextBox.UseWaitCursor = true;
             // 
             // authLabel
             // 
             this.authLabel.AutoSize = true;
-            this.authLabel.Location = new System.Drawing.Point(232, 231);
-            this.authLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.authLabel.Location = new System.Drawing.Point(309, 284);
             this.authLabel.Name = "authLabel";
-            this.authLabel.Size = new System.Drawing.Size(0, 13);
+            this.authLabel.Size = new System.Drawing.Size(0, 17);
             this.authLabel.TabIndex = 9;
             this.authLabel.UseWaitCursor = true;
             this.authLabel.Visible = false;
@@ -119,10 +117,9 @@ namespace OpenTTLauncher
             this.populationLabel.BackColor = System.Drawing.Color.Transparent;
             this.populationLabel.Font = new System.Drawing.Font("Impress BT", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.populationLabel.ForeColor = System.Drawing.Color.Black;
-            this.populationLabel.Location = new System.Drawing.Point(189, 378);
-            this.populationLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.populationLabel.Location = new System.Drawing.Point(252, 465);
             this.populationLabel.Name = "populationLabel";
-            this.populationLabel.Size = new System.Drawing.Size(109, 27);
+            this.populationLabel.Size = new System.Drawing.Size(138, 35);
             this.populationLabel.TabIndex = 11;
             this.populationLabel.Text = "population";
             this.populationLabel.UseWaitCursor = true;
@@ -134,10 +131,9 @@ namespace OpenTTLauncher
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Impress BT", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.InactiveBorder;
-            this.label1.Location = new System.Drawing.Point(14, 187);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(19, 230);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(93, 24);
+            this.label1.Size = new System.Drawing.Size(119, 30);
             this.label1.TabIndex = 13;
             this.label1.Text = "Password:";
             this.label1.Click += new System.EventHandler(this.Label1_Click_3);
@@ -148,10 +144,9 @@ namespace OpenTTLauncher
             this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("Impress BT", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.SystemColors.InactiveBorder;
-            this.label2.Location = new System.Drawing.Point(13, 135);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(17, 166);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(95, 24);
+            this.label2.Size = new System.Drawing.Size(123, 30);
             this.label2.TabIndex = 14;
             this.label2.Text = "Username:";
             // 
@@ -160,10 +155,9 @@ namespace OpenTTLauncher
             this.label3.AutoSize = true;
             this.label3.BackColor = System.Drawing.Color.Transparent;
             this.label3.Font = new System.Drawing.Font("Impress BT", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(182, 7);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Location = new System.Drawing.Point(243, 9);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(395, 43);
+            this.label3.Size = new System.Drawing.Size(486, 54);
             this.label3.TabIndex = 15;
             this.label3.Text = "Open Toontown Launcher";
             this.label3.Click += new System.EventHandler(this.Label3_Click);
@@ -173,10 +167,9 @@ namespace OpenTTLauncher
             this.label4.AutoSize = true;
             this.label4.BackColor = System.Drawing.Color.Transparent;
             this.label4.Font = new System.Drawing.Font("Impress BT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(230, 52);
-            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Location = new System.Drawing.Point(307, 64);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(302, 20);
+            this.label4.Size = new System.Drawing.Size(388, 25);
             this.label4.TabIndex = 16;
             this.label4.Text = "An open source unoffical Toontown launcher";
             // 
@@ -185,24 +178,23 @@ namespace OpenTTLauncher
             this.label5.AutoSize = true;
             this.label5.BackColor = System.Drawing.Color.Transparent;
             this.label5.Font = new System.Drawing.Font("Impress BT", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(2, 377);
-            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Location = new System.Drawing.Point(3, 464);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(193, 27);
+            this.label5.Size = new System.Drawing.Size(244, 35);
             this.label5.TabIndex = 17;
             this.label5.Text = "Current Population:";
             this.label5.Click += new System.EventHandler(this.Label5_Click);
             // 
             // instructionsButton
             // 
-            this.instructionsButton.BackColor = System.Drawing.Color.Transparent;
+            this.instructionsButton.BackColor = System.Drawing.Color.White;
             this.instructionsButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.instructionsButton.Font = new System.Drawing.Font("Impress BT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.instructionsButton.ForeColor = System.Drawing.Color.Black;
-            this.instructionsButton.Location = new System.Drawing.Point(322, 276);
-            this.instructionsButton.Margin = new System.Windows.Forms.Padding(2);
+            this.instructionsButton.Location = new System.Drawing.Point(429, 340);
+            this.instructionsButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.instructionsButton.Name = "instructionsButton";
-            this.instructionsButton.Size = new System.Drawing.Size(97, 31);
+            this.instructionsButton.Size = new System.Drawing.Size(129, 38);
             this.instructionsButton.TabIndex = 18;
             this.instructionsButton.Text = "Instructions";
             this.instructionsButton.UseVisualStyleBackColor = false;
@@ -210,33 +202,40 @@ namespace OpenTTLauncher
             // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.Color.White;
             this.button1.Font = new System.Drawing.Font("Impress BT", 10F);
-            this.button1.Location = new System.Drawing.Point(140, 231);
+            this.button1.Location = new System.Drawing.Point(187, 284);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(104, 30);
+            this.button1.Size = new System.Drawing.Size(139, 37);
             this.button1.TabIndex = 19;
             this.button1.Text = "Save Account";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.Button1_Click);
             // 
             // button2
             // 
+            this.button2.BackColor = System.Drawing.Color.White;
             this.button2.Font = new System.Drawing.Font("Impress BT", 10F);
-            this.button2.Location = new System.Drawing.Point(376, 231);
+            this.button2.Location = new System.Drawing.Point(501, 284);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(96, 30);
+            this.button2.Size = new System.Drawing.Size(128, 37);
             this.button2.TabIndex = 20;
             this.button2.Text = "QuickLogin";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.Button2_Click);
             // 
             // comboBox1
             // 
+            this.comboBox1.BackColor = System.Drawing.Color.White;
             this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(478, 244);
+            this.comboBox1.Location = new System.Drawing.Point(637, 300);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.Size = new System.Drawing.Size(160, 24);
             this.comboBox1.TabIndex = 21;
             this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.ComboBox1_SelectedIndexChanged);
             // 
@@ -245,33 +244,36 @@ namespace OpenTTLauncher
             this.label6.AutoSize = true;
             this.label6.BackColor = System.Drawing.Color.WhiteSmoke;
             this.label6.Font = new System.Drawing.Font("Impress BT", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(484, 223);
+            this.label6.Location = new System.Drawing.Point(645, 274);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(107, 16);
+            this.label6.Size = new System.Drawing.Size(142, 21);
             this.label6.TabIndex = 22;
             this.label6.Text = "Select An Account";
             this.label6.Click += new System.EventHandler(this.Label6_Click);
             // 
             // button3
             // 
+            this.button3.BackColor = System.Drawing.Color.White;
             this.button3.Font = new System.Drawing.Font("Impress BT", 10F);
-            this.button3.Location = new System.Drawing.Point(251, 231);
+            this.button3.Location = new System.Drawing.Point(335, 284);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(118, 30);
+            this.button3.Size = new System.Drawing.Size(157, 37);
             this.button3.TabIndex = 23;
             this.button3.Text = "Remove Account";
-            this.button3.UseVisualStyleBackColor = true;
+            this.button3.UseVisualStyleBackColor = false;
             this.button3.Click += new System.EventHandler(this.Button3_Click);
             // 
             // OpenTTLauncher
             // 
             this.AcceptButton = this.loginButton;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(742, 414);
+            this.ClientSize = new System.Drawing.Size(989, 510);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.comboBox1);
@@ -293,7 +295,7 @@ namespace OpenTTLauncher
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.KeyPreview = true;
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "OpenTTLauncher";
